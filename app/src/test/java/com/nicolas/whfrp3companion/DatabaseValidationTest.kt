90542d328d45c8b3d8ac8ff160df494684fca3e5
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-03-13 16:34:46 +0100

    update build.gradle of app, remove android instrumentation database test

---
+++ app/src/test/java/com/nicolas/whfrp3companion/DatabaseValidationTest.kt
@@ -0,0 +1,39 @@
+package com.nicolas.whfrp3companion
+
+import com.nicolas.whfrp3database.PlayerFacade
+import com.nicolas.whfrp3database.database
+import com.nicolas.whfrp3database.entities.player.Player
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
+class DatabaseValidationTest {
+    private lateinit var playerFacade: PlayerFacade
+
+    @Before
+    fun setUp() {
+        playerFacade = PlayerFacade(RuntimeEnvironment.application)
+    }
+
+    @Test
+    fun should_list_players() {
+        playerFacade.add(Player("John"))
+
+        val players = playerFacade.findAll()
+        assertThat(players).isNotEmpty
+        assertThat(players[0].name).isEqualTo("John")
+    }
+
+    @After
+    fun tearDown() {
+        RuntimeEnvironment.application.database.close()
+        RuntimeEnvironment.application.database.deleteDatabase()
+    }
+}
