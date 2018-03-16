a8f0d7dff7f45dc0c4f7b21e2868d11be16e1f9b
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-03-16 16:54:11 +0100

    trying the constraint layout (commented for the moment)

--- app/src/main/java/com/nicolas/whfrp3companion/DiceLauncherActivity.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/DiceLauncherActivity.kt
@@ -6,14 +6,38 @@ import android.support.v7.widget.Toolbar
 import butterknife.ButterKnife
 import butterknife.OnClick
 import butterknife.Unbinder
+import com.nicolas.whfrp3database.entities.hand.Hand
 import org.jetbrains.anko.longToast
 
 class DiceLauncherActivity : AppCompatActivity() {
+//    @BindView(R.id.characteristic_dice_picker)
+//    lateinit var characteristicDicePicker: NumberPicker
+//    @BindView(R.id.conservative_dice_picker)
+//    lateinit var conservativeDicePicker: NumberPicker
+//    @BindView(R.id.reckless_dice_picker)
+//    lateinit var recklessDicePicker: NumberPicker
+//    @BindView(R.id.expertise_dice_picker)
+//    lateinit var expertiseDicePicker: NumberPicker
+//    @BindView(R.id.fortune_dice_picker)
+//    lateinit var fortuneDicePicker: NumberPicker
+//    @BindView(R.id.challenge_dice_picker)
+//    lateinit var challengeDicePicker: NumberPicker
+//    @BindView(R.id.misfortune_dice_picker)
+//    lateinit var misfortuneDicePicker: NumberPicker
+
     private lateinit var unbinder: Unbinder
+
+    private lateinit var hand: Hand
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_launcher)
+
+        hand = if (intent?.extras !== null) {
+            intent.extras.getSerializable(HAND_INTENT_ARGUMENT) as Hand
+        } else {
+            Hand("")
+        }
 
         unbinder = ButterKnife.bind(this)
 
@@ -28,6 +52,16 @@ class DiceLauncherActivity : AppCompatActivity() {
 
     @OnClick(R.id.fab_launch_hand)
     fun launchHand() {
+//        val hand = Hand("No name",
+//                characteristicDicesCount = characteristicDicePicker.value,
+//                expertiseDicesCount = expertiseDicePicker.value,
+//                fortuneDicesCount = fortuneDicePicker.value,
+//                conservativeDicesCount = conservativeDicePicker.value,
+//                recklessDicesCount = recklessDicePicker.value,
+//                challengeDicesCount = challengeDicePicker.value,
+//                misfortuneDicesCount = misfortuneDicePicker.value)
+//        longToast(hand.toString())
+
         longToast("You wanted to launch something, right ?")
     }
 
--- app/src/main/java/com/nicolas/whfrp3companion/IntentArguments.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/IntentArguments.kt
@@ -1,3 +1,5 @@
 package com.nicolas.whfrp3companion
 
 const val PLAYER_INTENT_ARGUMENT = "player"
+
+const val HAND_INTENT_ARGUMENT = "hand"
