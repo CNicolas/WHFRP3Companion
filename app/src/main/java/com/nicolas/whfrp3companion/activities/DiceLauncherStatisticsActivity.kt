25d0fe6d85301dfc34105425c78611c01873e768
Clément NICOLAS <CNicolas@users.noreply.github.com>
2018-03-17 10:37:27 +0100

    creating dice launcher activity

---
+++ app/src/main/java/com/nicolas/whfrp3companion/activities/DiceLauncherStatisticsActivity.kt
@@ -0,0 +1,38 @@
+package com.nicolas.whfrp3companion.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import butterknife.ButterKnife
+import butterknife.Unbinder
+import com.nicolas.whfrp3companion.HAND_INTENT_ARGUMENT
+import com.nicolas.whfrp3companion.R
+import com.nicolas.whfrp3database.entities.hand.Hand
+
+class DiceLauncherStatisticsActivity : AppCompatActivity() {
+    private lateinit var unbinder: Unbinder
+    private lateinit var hand: Hand
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dice_launcher)
+
+        unbinder = ButterKnife.bind(this)
+
+        hand = if (intent?.extras !== null) {
+            intent.extras.getSerializable(HAND_INTENT_ARGUMENT) as Hand
+        } else {
+            Hand("")
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbinder.unbind()
+    }
+}
