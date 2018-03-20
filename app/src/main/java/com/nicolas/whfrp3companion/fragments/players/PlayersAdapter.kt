29ab039b387f42d5df2433cb84e0869906136ac7
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-03-20 14:33:47 +0100

    set gravity for players list

--- app/src/main/java/com/nicolas/whfrp3companion/fragments/players/PlayersAdapter.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/fragments/players/PlayersAdapter.kt
@@ -1,8 +1,10 @@
 package com.nicolas.whfrp3companion.fragments.players
 
 import android.content.Context
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.TEXT_ALIGNMENT_CENTER
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
@@ -42,6 +44,8 @@ class PlayersAdapter(context: Context, private val players: List<Player>) : Base
 
         init {
             ButterKnife.bind(this, view)
+            name.gravity = Gravity.CENTER
+            name.textAlignment = TEXT_ALIGNMENT_CENTER
         }
     }
 }
