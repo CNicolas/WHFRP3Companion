ea2f500d96e2776e303349057c6f401b3c3922cb
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-03-26 11:42:16 +0200

    lint

--- app/src/main/java/com/nicolas/whfrp3companion/shared/enums/EnumSpinnerElement.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/shared/enums/EnumSpinnerElement.kt
@@ -2,22 +2,24 @@ package com.nicolas.whfrp3companion.shared.enums
 
 import com.nicolas.whfrp3companion.R
 import com.nicolas.whfrp3database.entities.player.enums.Characteristic
+import com.nicolas.whfrp3database.entities.player.enums.Characteristic.*
 import com.nicolas.whfrp3database.entities.player.enums.Race
+import com.nicolas.whfrp3database.entities.player.enums.Race.*
 
 internal val Race.labelId: Int
     get() = when (this) {
-        Race.HUMAN -> R.string.human
-        Race.DWARF -> R.string.dwarf
-        Race.HIGH_ELF -> R.string.high_elf
-        Race.WOOD_ELF -> R.string.wood_elf
+        HUMAN -> R.string.human
+        DWARF -> R.string.dwarf
+        HIGH_ELF -> R.string.high_elf
+        WOOD_ELF -> R.string.wood_elf
     }
 
 internal val Characteristic.labelId: Int
     get() = when (this) {
-        Characteristic.STRENGTH -> R.string.strength
-        Characteristic.TOUGHNESS -> R.string.toughness
-        Characteristic.AGILITY -> R.string.agility
-        Characteristic.INTELLIGENCE -> R.string.intelligence
-        Characteristic.WILLPOWER -> R.string.willpower
-        Characteristic.FELLOWSHIP -> R.string.fellowship
+        STRENGTH -> R.string.strength
+        TOUGHNESS -> R.string.toughness
+        AGILITY -> R.string.agility
+        INTELLIGENCE -> R.string.intelligence
+        WILLPOWER -> R.string.willpower
+        FELLOWSHIP -> R.string.fellowship
     }
