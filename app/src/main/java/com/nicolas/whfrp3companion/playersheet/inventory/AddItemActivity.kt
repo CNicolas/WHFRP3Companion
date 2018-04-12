5ae34e919265552b2cdb89dd7a3e590a4119b882
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-04-12 16:26:21 +0200

    WIP(app-inventory): Fill enum spinners (quality, armor type, weapon type)

--- app/src/main/java/com/nicolas/whfrp3companion/playersheet/inventory/AddItemActivity.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/playersheet/inventory/AddItemActivity.kt
@@ -3,65 +3,67 @@ package com.nicolas.whfrp3companion.playersheet.inventory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.text.Editable
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.NumberPicker
-import android.widget.Spinner
-import butterknife.*
+import android.widget.*
+import butterknife.ButterKnife
+import butterknife.OnClick
+import butterknife.OnItemSelected
+import butterknife.Unbinder
 import com.nicolas.whfrp3companion.R
 import com.nicolas.whfrp3companion.shared.PLAYER_NAME_INTENT_ARGUMENT
 import com.nicolas.whfrp3companion.shared.bind
 import com.nicolas.whfrp3companion.shared.enums.labelId
+import com.nicolas.whfrp3companion.shared.intValue
 import com.nicolas.whfrp3database.PlayerFacade
 import com.nicolas.whfrp3database.entities.player.Player
-import com.nicolas.whfrp3database.entities.player.playerLinked.item.GenericItem
-import com.nicolas.whfrp3database.entities.player.playerLinked.item.Item
-import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.ItemType
+import com.nicolas.whfrp3database.entities.player.playerLinked.item.*
+import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.*
 import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.ItemType.*
-import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.Quality
 import com.nicolas.whfrp3database.extensions.addItem
-import com.nicolas.whfrp3database.extensions.moveToItemType
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class AddItemActivity : AppCompatActivity() {
+    private val itemNameEditText by bind<EditText>(R.id.item_name)
     private val itemTypeView by bind<Spinner>(R.id.item_type)
-    private val qualityView by bind<Spinner>(R.id.quality)
+    private val encumbranceEditText by bind<EditText>(R.id.encumbrance)
+    private val quantityEditText by bind<EditText>(R.id.quantity)
+    private val qualitySpinner by bind<Spinner>(R.id.quality)
+    private val descriptionEditText by bind<EditText>(R.id.description)
 
     private val armorLayout by bind<View>(R.id.armor_layout)
+    private val armorTypeSpinner by bind<Spinner>(R.id.armor_type)
     private val defenseLabelView by bind<ImageView>(R.id.defense_label)
-    private val defenseView by bind<NumberPicker>(R.id.defense)
+    private val defenseNumberPicker by bind<NumberPicker>(R.id.defense)
     private val soakLabelView by bind<ImageView>(R.id.soak_label)
-    private val soakView by bind<NumberPicker>(R.id.soak)
+    private val soakNumberPicker by bind<NumberPicker>(R.id.soak)
 
     private val expandableLayout by bind<View>(R.id.expandable_layout)
     private val usesLabelView by bind<ImageView>(R.id.uses_label)
-    private val usesView by bind<NumberPicker>(R.id.uses)
+    private val usesNumberPicker by bind<NumberPicker>(R.id.uses)
 
     private val weaponLayout by bind<View>(R.id.weapon_layout)
-    private val weaponTypeView by bind<Spinner>(R.id.weapon_type)
+    private val weaponTypeSpinner by bind<Spinner>(R.id.weapon_type)
+    private val weaponRangeSpinner by bind<Spinner>(R.id.range)
     private val damageLabelView by bind<ImageView>(R.id.damage_label)
-    private val damageView by bind<NumberPicker>(R.id.damage)
+    private val damageNumberPicker by bind<NumberPicker>(R.id.damage)
     private val criticalLevelLabelView by bind<ImageView>(R.id.critical_level_label)
-    private val criticalLevelView by bind<NumberPicker>(R.id.critical_level)
+    private val criticalLevelNumberPicker by bind<NumberPicker>(R.id.critical_level)
 
     private lateinit var unbinder: Unbinder
 
     private val armorViews
-        get () = listOf(armorLayout, defenseLabelView, defenseView, soakLabelView, soakView)
+        get () = listOf(armorLayout, armorTypeSpinner, defenseLabelView, defenseNumberPicker, soakLabelView, soakNumberPicker)
     private val expandableViews
-        get() = listOf(expandableLayout, usesLabelView, usesView)
+        get() = listOf(expandableLayout, usesLabelView, usesNumberPicker)
     private val weaponViews
-        get () = listOf(weaponLayout, weaponTypeView, damageLabelView, damageView, criticalLevelLabelView, criticalLevelView)
+        get () = listOf(weaponLayout, weaponTypeSpinner, weaponRangeSpinner, damageLabelView, damageNumberPicker, criticalLevelLabelView, criticalLevelNumberPicker)
 
     private lateinit var playerFacade: PlayerFacade
 
     private lateinit var player: Player
-    private var item: Item = GenericItem("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,11 +77,20 @@ class AddItemActivity : AppCompatActivity() {
             player = playerFacade.find(playerName)!!
         }
 
-        itemTypeView.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, values().map { getString(it.labelId) })
+        itemTypeView.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, ItemType.values().map { getString(it.labelId) })
         itemTypeView.setSelection(ItemType.GENERIC_ITEM.ordinal)
 
-        qualityView.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, Quality.values().map { getString(it.labelId) })
-        qualityView.setSelection(Quality.NORMAL.ordinal)
+        qualitySpinner.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, Quality.values().map { getString(it.labelId) })
+        qualitySpinner.setSelection(Quality.NORMAL.ordinal)
+
+        armorTypeSpinner.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, ArmorType.values().map { getString(it.labelId) })
+        armorTypeSpinner.setSelection(ArmorType.LEATHER.ordinal)
+
+        weaponTypeSpinner.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, WeaponType.values().map { getString(it.labelId) })
+        weaponTypeSpinner.setSelection(WeaponType.SWORD.ordinal)
+
+        weaponRangeSpinner.adapter = ArrayAdapter(this, R.layout.element_enum_spinner, Range.values().map { getString(it.labelId) })
+        weaponRangeSpinner.setSelection(Range.ENGAGED.ordinal)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -90,17 +101,9 @@ class AddItemActivity : AppCompatActivity() {
         unbinder.unbind()
     }
 
-    @OnTextChanged(R.id.item_name,
-            callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
-    fun modifyItem(editable: Editable) {
-        item.name = editable.trim().toString()
-    }
-
     @OnItemSelected(R.id.item_type)
     fun selectItemType(spinner: Spinner, position: Int) {
-        item = item.moveToItemType(ItemType[position])
-
-        when (item.type) {
+        when (ItemType[position]) {
             ARMOR -> showArmorViews()
             EXPANDABLE -> showExpandableViews()
             GENERIC_ITEM -> showGenericItemViews()
@@ -111,7 +114,7 @@ class AddItemActivity : AppCompatActivity() {
     @OnClick(R.id.save_item)
     fun save() {
         doAsync {
-            player.addItem(item)
+            player.addItem(createItemFromViews())
             player = playerFacade.update(player)
 
             uiThread {
@@ -119,6 +122,48 @@ class AddItemActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun createItemFromViews(): Item =
+            when (ItemType[itemTypeView.selectedItemPosition]) {
+                ARMOR -> Armor(
+                        itemNameEditText.text.toString(),
+                        descriptionEditText.text.toString(),
+                        encumbranceEditText.intValue,
+                        quantityEditText.intValue,
+                        Quality[qualitySpinner.selectedItemPosition],
+                        false,
+                        ArmorType[armorTypeSpinner.selectedItemPosition],
+                        soakNumberPicker.value,
+                        defenseNumberPicker.value
+                )
+                EXPANDABLE -> Expandable(
+                        itemNameEditText.text.toString(),
+                        descriptionEditText.text.toString(),
+                        encumbranceEditText.intValue,
+                        quantityEditText.intValue,
+                        Quality[qualitySpinner.selectedItemPosition],
+                        usesNumberPicker.value
+                )
+                GENERIC_ITEM -> GenericItem(
+                        itemNameEditText.text.toString(),
+                        descriptionEditText.text.toString(),
+                        encumbranceEditText.intValue,
+                        quantityEditText.intValue,
+                        Quality[qualitySpinner.selectedItemPosition]
+                )
+                WEAPON -> Weapon(
+                        itemNameEditText.text.toString(),
+                        descriptionEditText.text.toString(),
+                        encumbranceEditText.intValue,
+                        quantityEditText.intValue,
+                        Quality[qualitySpinner.selectedItemPosition],
+                        false,
+                        WeaponType[weaponTypeSpinner.selectedItemPosition],
+                        damageNumberPicker.value,
+                        criticalLevelNumberPicker.value,
+                        Range[weaponRangeSpinner.selectedItemPosition]
+                )
+            }
 
     private fun showArmorViews() {
         armorViews.show()
--- app/src/main/java/com/nicolas/whfrp3companion/shared/enums/EnumSpinnerElement.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/shared/enums/EnumSpinnerElement.kt
@@ -5,9 +5,11 @@ import com.nicolas.whfrp3database.entities.player.enums.Characteristic
 import com.nicolas.whfrp3database.entities.player.enums.Characteristic.*
 import com.nicolas.whfrp3database.entities.player.enums.Race
 import com.nicolas.whfrp3database.entities.player.enums.Race.*
-import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.ItemType
-import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.Quality
+import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.*
+import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.ArmorType.*
 import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.Quality.*
+import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.Range.*
+import com.nicolas.whfrp3database.entities.player.playerLinked.item.enums.WeaponType.*
 
 internal val Race.labelId: Int
     get() = when (this) {
@@ -48,4 +50,48 @@ internal val Quality.labelId: Int
         NORMAL -> R.string.quality_normal
         SUPERIOR -> R.string.quality_superior
         MAGIC -> R.string.quality_magic
+    }
+
+internal val ArmorType.labelId: Int
+    get() = when (this) {
+        HELMET -> R.string.armor_helmet
+        HAT -> R.string.armor_hat
+        PLATE -> R.string.armor_plate
+        MAIL -> R.string.armor_mail
+        LEATHER -> R.string.armor_leather
+        ROBE -> R.string.armor_robe
+        SHIELD -> R.string.armor_shield
+    }
+
+internal val WeaponType.labelId: Int
+    get() = when (this) {
+        DAGGER -> R.string.weapon_dagger
+        FLAIL -> R.string.weapon_flail
+        HALBERD -> R.string.weapon_halberd
+        HAMMER -> R.string.weapon_hammer
+        TWO_HANDED_HAMMER -> R.string.weapon_two_handed_hammer
+        MACE -> R.string.weapon_mace
+        TWO_HANDED_MACE -> R.string.weapon_two_handed_mace
+        SPEAR -> R.string.weapon_spear
+        STICK -> R.string.weapon_stick
+        SWORD -> R.string.weapon_sword
+        TWO_HANDED_SWORD -> R.string.weapon_two_handed_sword
+        WHIP -> R.string.weapon_whip
+        BOW -> R.string.weapon_bow
+        CROSSBOW -> R.string.weapon_crossbow
+        ONE_HANDED_CROSSBOW -> R.string.weapon_one_handed_crossbow
+        SLING -> R.string.weapon_sling
+        HANDGUN -> R.string.weapon_handgun
+        RIFLE -> R.string.weapon_rifle
+        REPEATING_GUN -> R.string.weapon_repeating_gun
+        REPEATING_CROSSBOW -> R.string.weapon_repeating_crossbow
+    }
+
+internal val Range.labelId: Int
+    get() = when (this) {
+        ENGAGED -> R.string.range_engaged
+        SHORT -> R.string.range_short
+        MEDIUM -> R.string.range_medium
+        LONG -> R.string.range_long
+        EXTREME -> R.string.range_extreme
     }
