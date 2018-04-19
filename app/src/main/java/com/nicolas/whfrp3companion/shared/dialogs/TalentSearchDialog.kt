d1a094e59142b761bd9ee3a1acda9a7320763311
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-04-19 16:52:08 +0200

    WIP(app-talents): simplify talents search

--- app/src/main/java/com/nicolas/whfrp3companion/shared/dialogs/TalentSearchDialog.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/shared/dialogs/TalentSearchDialog.kt
@@ -11,26 +11,22 @@ import android.widget.Spinner
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.Unbinder
+import com.nicolas.playersheet.dtos.TalentSearch
 import com.nicolas.whfrp3companion.R
+import com.nicolas.whfrp3companion.shared.TALENTS_SEARCH_INTENT_ARGUMENT
+import com.nicolas.whfrp3companion.shared.activities.TalentsActivity
 import com.nicolas.whfrp3companion.shared.enums.labelId
-import com.nicolas.whfrp3database.entities.player.enums.Characteristic
 import com.nicolas.whfrp3database.entities.player.playerLinked.talent.TalentCooldown
 import com.nicolas.whfrp3database.entities.player.playerLinked.talent.TalentType
-import com.nicolas.whfrp3database.staticData.loadSkills
+import org.jetbrains.anko.intentFor
 
 class TalentSearchDialog : DialogFragment() {
     @BindView(R.id.talent_type)
     lateinit var talentTypeSpinner: Spinner
     @BindView(R.id.cooldown)
     lateinit var cooldownSpinner: Spinner
-    @BindView(R.id.talent_name)
-    lateinit var talentNameEditText: EditText
-    @BindView(R.id.description)
-    lateinit var descriptionEditText: EditText
-    @BindView(R.id.characteristic)
-    lateinit var characteristicSpinner: Spinner
-    @BindView(R.id.skill)
-    lateinit var skillsSpinner: Spinner
+    @BindView(R.id.search)
+    lateinit var searchEditText: EditText
 
     private lateinit var unbinder: Unbinder
 
@@ -42,21 +38,41 @@ class TalentSearchDialog : DialogFragment() {
 
         builder.setView(view)
         builder.setTitle(R.string.search)
-        builder.setPositiveButton(R.string.search, { _, _ -> dismiss() })
+        builder.setPositiveButton(R.string.search, { _, _ ->
+            if (activity != null) {
+                startActivity(activity!!.intentFor<TalentsActivity>(
+                        TALENTS_SEARCH_INTENT_ARGUMENT to getTalentSearchFromViews()
+                ))
+                dismiss()
+            }
+        })
 
         val dialog = builder.create()
         unbinder = ButterKnife.bind(this, view)
 
         val talentTypes = listOf(getString(R.string.all)) + TalentType.values().map { getString(it.labelId) }
         val talentCooldowns = listOf(getString(R.string.all)) + TalentCooldown.values().map { getString(it.labelId) }
-        val characteristics = listOf(getString(R.string.all)) + Characteristic.values().map { getString(it.labelId) }
-        val skills = listOf(getString(R.string.all)) + loadSkills(context!!).map { it.name }
 
         talentTypeSpinner.adapter = ArrayAdapter(context, R.layout.element_enum_spinner, talentTypes)
         cooldownSpinner.adapter = ArrayAdapter(context, R.layout.element_enum_spinner, talentCooldowns)
-        characteristicSpinner.adapter = ArrayAdapter(context, R.layout.element_enum_spinner, characteristics)
-        skillsSpinner.adapter = ArrayAdapter(context, R.layout.element_enum_spinner, skills)
 
         return dialog
+    }
+
+    private fun getTalentSearchFromViews(): TalentSearch {
+        val talentType = when {
+            talentTypeSpinner.selectedItemPosition == 0 -> null
+            else -> TalentType[talentTypeSpinner.selectedItemPosition + 1]
+        }
+
+        val talentCooldown = when {
+            cooldownSpinner.selectedItemPosition == 0 -> null
+            else -> TalentCooldown[cooldownSpinner.selectedItemPosition + 1]
+        }
+
+        return TalentSearch(
+                text = searchEditText.text.toString(),
+                talentType = talentType,
+                cooldown = talentCooldown)
     }
 }
