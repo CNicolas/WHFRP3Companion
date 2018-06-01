7246e6df25cfeac244fef43d53927b17ab06896d
Clément NICOLAS <CNicolas@users.noreply.github.com>
2018-06-01 22:30:39 +0200

    feat(effects-search): Search on a list of effects

--- app/src/main/java/com/nicolas/whfrp3companion/playersheet/state/EffectsActivity.kt
+++ app/src/main/java/com/nicolas/whfrp3companion/playersheet/state/EffectsActivity.kt
@@ -5,10 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.widget.ArrayAdapter
-import android.widget.SearchView
 import com.nicolas.models.player.effect.Effect
 import com.nicolas.whfrp3companion.R
 import kotlinx.android.synthetic.main.activity_effects.*
@@ -28,15 +28,6 @@ class EffectsActivity internal constructor() : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        handleIntent(intent)
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        setIntent(intent)
-        handleIntent(intent)
-    }
-
-    private fun handleIntent(intent: Intent?) {
         intent?.let {
             val effects = if (Intent.ACTION_SEARCH == intent.action) {
                 val query = intent.getStringExtra(SearchManager.QUERY)
@@ -45,7 +36,7 @@ class EffectsActivity internal constructor() : AppCompatActivity() {
                 allEffects.toList()
             }
 
-            effectsListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, effects.map { it.name })
+            updateEffectsList(effects)
         }
     }
 
@@ -57,13 +48,30 @@ class EffectsActivity internal constructor() : AppCompatActivity() {
 
             val searchManager: SearchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
             val info = searchManager.getSearchableInfo(componentName)
-            val searchView: SearchView = searchMenuItem.actionView as SearchView
+            val searchView: SearchView? = searchMenuItem.actionView as SearchView?
 
-            searchView.setSearchableInfo(info)
-            searchView.setIconifiedByDefault(false)
+            searchView?.let {
+                searchView.setSearchableInfo(info)
+                searchView.setIconifiedByDefault(false)
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        updateEffectsList(searchEffectByName(query))
+                        return false
+                    }
+
+                    override fun onQueryTextChange(s: String): Boolean {
+                        updateEffectsList(searchEffectByName(s))
+                        return false
+                    }
+                })
+            }
         }
 
-        return true
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun updateEffectsList(effects: List<Effect>) {
+        effectsListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, effects.map { it.name })
     }
 
     private fun searchEffectByName(searchText: String): List<Effect> {
